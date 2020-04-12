1976d93e7431a11cba0fdb2020041ebc737f0d35
yohan <783b8c87@scimetis.net>
2020-04-12 16:15:47 +0200

    Completed backup_ovh1 script.

--- backup_scripts/backup_ovh1.sh
+++ backup_scripts/backup_ovh1.sh
@@ -13,6 +13,18 @@ export SWIFT_TENANTNAME=$OS_TENANT_NAME
 export SWIFT_REGIONNAME=$OS_REGION_NAME
 export PASSPHRASE=$DUPLICITY_PASSPHRASE
 
-ls -l /mnt/volumes
-#duplicity --num-retries 3 --full-if-older-than 1M --progress --archive-dir ${ARCHIVE_DIR} --name test --allow-source-mismatch /usr swift://Backup
-#duplicity remove-older-than 2M --archive-dir ${ARCHIVE_DIR} --name test --allow-source-mismatch --force swift://Backup
+duplicity --num-retries 3 --full-if-older-than 1M --progress \
+    --archive-dir ${ARCHIVE_DIR} --name backup_ovh1 \
+    --allow-source-mismatch \
+    --include /mnt/volumes/elasticsearch_data \
+    --include /mnt/volumes/gogs_data \
+    --include /mnt/volumes/mail_data \
+    --include /mnt/volumes/mysql-server_dumps \
+    --include /mnt/volumes/nextcloud \
+    --include /mnt/volumes/reverse-proxy_conf \
+    --include /mnt/volumes/reverse-proxy_conf_enabled \
+    --include /mnt/volumes/reverse-proxy_letsencrypt \
+    --include /mnt/volumes/scuttle_code \
+    --include /mnt/volumes/scuttle_php5-fpm_conf \
+    --exclude '**' /mnt/volumes swift://backup_ovh1
+duplicity remove-older-than 2M --archive-dir ${ARCHIVE_DIR} --name backup_ovh1 --allow-source-mismatch --force swift://backup_ovh1
