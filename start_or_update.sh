6091934f814584d3ee73ba3690c01b32dccbf87f
yohan-b <yohan-b@users.noreply.github.com>
2021-06-06 19:27:18 +0200

    Update start_or_update.sh to exit if volume cannot be created.

--- start_or_update.sh
+++ start_or_update.sh
@@ -60,7 +60,7 @@ VOLUME=tmp_duplicity_workdir
 sudo mkdir -p /mnt/volumes/${VOLUME}
 if ! mountpoint -q /mnt/volumes/${VOLUME}
 then
-     ~/env_py3/bin/openstack volume create ${VOLUME} --size 20 --type high-speed
+     ~/env_py3/bin/openstack volume create ${VOLUME} --size 20 --type high-speed || exit 1
      VOLUME_ID=$(~/env_py3/bin/openstack volume show ${VOLUME} -c id --format value)
      test -e /dev/disk/by-id/*${VOLUME_ID:0:20} || nova volume-attach $INSTANCE_OPENSTACK $VOLUME_ID auto
      sleep 3
@@ -72,7 +72,7 @@ VOLUME=duplicity_cache
 sudo mkdir -p /mnt/volumes/${VOLUME}
 if ! mountpoint -q /mnt/volumes/${VOLUME}
 then
-     ~/env_py3/bin/openstack volume create ${VOLUME} --size 2 --type high-speed
+     ~/env_py3/bin/openstack volume create ${VOLUME} --size 2 --type high-speed || exit 1
      VOLUME_ID=$(~/env_py3/bin/openstack volume show ${VOLUME} -c id --format value)
      test -e /dev/disk/by-id/*${VOLUME_ID:0:20} || nova volume-attach $INSTANCE_OPENSTACK $VOLUME_ID auto
      sleep 3
